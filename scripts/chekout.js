27e8e644ff4a31bd30c4703b57c16bad63631fd5
Karthikeyanr12 <karthikeyanramanujamr@gmail.com>
2026-03-10 22:05:30 +0530

    Learn Promises and covert code to promises

--- scripts/chekout.js
+++ scripts/chekout.js
@@ -1,11 +1,51 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import "../data/cart-class.js"
 //import '../data/car.js';
 //import '../data/backend-practice.js'
 
-loadProducts(()=>{
+Promise.all([
+    new Promise((resolve)=>{
+    loadProducts(()=>{
+            resolve('Hello');
+        });
+    }),
+    new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        })
+    })
+
+]).then(()=>{
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+/*
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve('Hello');
+    });
+}).then((value)=>{
+    console.log(value)
+    return new Promise((resolve)=>{
+        loadCart(()=>{
+            resolve();
+        })
+    }).then(()=>{
+        renderOrderSummary();
+        renderPaymentSummary();
+    })
+});
+*/
+
+/*
+loadProducts(()=>{
+    loadCart(()=>{
+        renderOrderSummary();
+        renderPaymentSummary();
+    })
+});
+*/
