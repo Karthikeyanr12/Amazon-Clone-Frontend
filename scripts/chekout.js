7591d438454f72a6f51fb32365130744f052fdf1
Karthikeyanr12 <karthikeyanramanujamr@gmail.com>
2026-03-12 00:25:50 +0530

    Complete lesson 18 exercises till k

--- scripts/chekout.js
+++ scripts/chekout.js
@@ -1,7 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts,loadProductsFetch } from "../data/products.js";
-import { loadCart } from "../data/cart.js";
+import { loadCart,loadCartFetch } from "../data/cart.js";
 //import "../data/cart-class.js"
 //import '../data/car.js';
 //import '../data/backend-practice.js'
@@ -9,18 +9,26 @@ import { loadCart } from "../data/cart.js";
 async function loadPage(){
 
     try{
+        await Promise.all([loadProductsFetch(),loadCartFetch()])
+    }catch(error){
+        console.log('Unexpected Error. Please try again')
+    }
+
+    
+    /*
+    try{
         //throw 'error1';
         await loadProductsFetch();
         const value = await new Promise((resolve,reject)=>{
             //throw 'error2'
-            loadCart(()=>{
+            loadCartFetch(()=>{
                 //reject('error3')
                 resolve('Hello');
             });
         });
     }catch(error){
         console.log('Unexpexted Error. Please try again later.')
-    }
+    }*/
     
 
     renderOrderSummary();
